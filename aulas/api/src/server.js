d137d081a4d7fc78734a81a04987afd8b1cf8089
João Gabriel <joaogabriel9633@gmail.com>
2025-09-03 06:53:32 -0300

    Finish api

--- aulas/api/src/server.js
+++ aulas/api/src/server.js
@@ -1,20 +1,11 @@
 import http from "node:http";
 import { jsonBodyHanlder } from "./middlewares/jsonHandler.js";
+import { routeHandler } from "./middlewares/routeHandler.js";
 
 const server = http.createServer(async (request, response) => {
-  const { method, url } = request;
-
   await jsonBodyHanlder(request, response);
 
-  if (method === "GET" && url === "/products") {
-    return response.end("Lista de produtos!");
-  }
-
-  if (method === "POST" && url === "/products") {
-    return response.writeHead(201).end(JSON.stringify(request.body));
-  }
-
-  return response.writeHead(404).end(`Método: ${method}/ URL: ${url}`);
+  routeHandler(request, response);
 });
 
 server.listen(3333, () => {
