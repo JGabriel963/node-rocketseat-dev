f964d547a0e9b5f7dc1acc47d8d8399330de3f59
João Gabriel <joaogabriel9633@gmail.com>
2025-11-19 21:29:16 -0300

    jwt auth

--- aulas/03-api-solid/src/app.ts
+++ aulas/03-api-solid/src/app.ts
@@ -1,6 +1,30 @@
+import fastifyJwt from "@fastify/jwt";
 import fastify from "fastify";
+import { ZodError } from "zod";
 import { appRoutes } from "./http/routes";
+import { env } from "./env";
 
 export const app = fastify();
 
+app.register(fastifyJwt, {
+  secret: env.JWT_SECRET,
+});
+
 app.register(appRoutes);
+
+app.setErrorHandler((error, _, reply) => {
+  if (error instanceof ZodError) {
+    return reply.status(400).send({
+      message: "Validation error",
+      issues: error.format(),
+    });
+  }
+
+  if (env.NODE_ENV !== "production") {
+    console.log(error);
+  } else {
+    ///
+  }
+
+  return reply.status(500).send({ message: "Internal server error." });
+});
