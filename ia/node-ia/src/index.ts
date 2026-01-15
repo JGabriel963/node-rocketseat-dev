3435f645c31d9e5a675d584b10c36dd555cda584
João Gabriel <joaogabriel9633@gmail.com>
2026-01-15 17:40:01 -0300

    zodFormated

--- ia/node-ia/src/index.ts
+++ ia/node-ia/src/index.ts
@@ -1,3 +1,42 @@
 import "dotenv/config";
+import app from "./app";
+import { z } from "zod";
+import OpenAI from "openai";
+import { zodResponseFormat } from "openai/helpers/zod";
 
-console.log("API Key", process.env.OPEN_AI_API_KEY);
+const client = new OpenAI({
+  apiKey: process.env.OPEN_AI_API_KEY,
+});
+
+const schema = z.object({
+  produtos: z.array(z.string()),
+});
+
+app.post("/generate", async (req, res) => {
+  try {
+    const completion = await client.chat.completions.parse({
+      model: "gpt-4o-mini",
+      max_completion_tokens: 100,
+      response_format: zodResponseFormat(schema, "produtos_schema"),
+      messages: [
+        {
+          role: "developer",
+          content: "Liste três produtos que atendam a necessidade do usuário.",
+        },
+        {
+          role: "user",
+          content: req.body.message,
+        },
+      ],
+    });
+
+    res.json(completion.choices[0].message.parsed?.produtos);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+app.listen(3333, () => {
+  console.log("Server is running on port 3333");
+});
