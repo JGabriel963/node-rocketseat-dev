518b5a474cd4a4dc68f2be6eb44b1b615964aa17
João Gabriel <joaogabriel9633@gmail.com>
2026-01-21 22:04:29 -0300

    update

--- ia/node-ia/src/openai.ts
+++ ia/node-ia/src/openai.ts
@@ -3,7 +3,7 @@ import { z } from "zod";
 import OpenAI from "openai";
 import { zodResponseFormat } from "openai/helpers/zod";
 import {
-  ChatCompletionMessageParam,
+  type ChatCompletionMessageParam,
   ChatCompletionTool,
 } from "openai/resources";
 import { produtosEmEstoque, produtosEmFalta } from "./database";
@@ -61,6 +61,38 @@ const generateCompletiion = async (
     throw new Error("Refusal");
   }
 
+  const { tool_calls } = completion.choices[0].message;
+
+  if (tool_calls) {
+    const [tool_call] = tool_calls;
+
+    const toolsMap = {
+      produtos_em_estoque: produtosEmEstoque,
+      produtos_em_falta: produtosEmFalta,
+    };
+
+    const functionToCall =
+      toolsMap[tool_call.function.name as keyof typeof toolsMap];
+
+    if (!functionToCall) {
+      throw new Error("Function not found");
+    }
+
+    const result = functionToCall();
+
+    messages.push(completion.choices[0].message);
+    messages.push({
+      role: "tool",
+      tool_call_id: tool_call.id,
+      content: result.toString(),
+    });
+
+    return await generateCompletiion(
+      messages,
+      zodResponseFormat(schema, "produtos_schema"),
+    );
+  }
+
   return completion;
 };
 
@@ -81,36 +113,6 @@ export const generateProducts = async (message: string) => {
     messages,
     zodResponseFormat(schema, "produtos_schema"),
   );
-
-  const { tool_calls } = completion.choices[0].message;
-
-  if (tool_calls) {
-    const [tool_call] = tool_calls;
-    const toolMap = {
-      produtos_em_estoque: produtosEmEstoque,
-      produtos_em_falta: produtosEmFalta,
-    };
-
-    const functionToCall =
-      toolMap[tool_call.function.name as keyof typeof toolMap];
-
-    if (!functionToCall) {
-      throw new Error("Function not found");
-    }
-    const result = functionToCall();
-    messages.push(completion.choices[0].message);
-    messages.push({
-      role: "tool",
-      tool_call_id: tool_call.id,
-      content: result.toString(),
-    });
-
-    const completion2 = await generateCompletiion(
-      messages,
-      zodResponseFormat(schema, "produtos_schema"),
-    );
-    return completion2.choices[0].message.parsed;
-  }
 
   return completion.choices[0].message.parsed;
 };
