2a1e4f8114a7ab50b3a3223a5f4d2a9cf3cb3187
João Gabriel <joaogabriel9633@gmail.com>
2026-03-27 14:31:18 -0300

    update use-cases

---
+++ aulas/04-clean-ddd/src/domain/forum/application/use-cases/comment-on-answer.spec.ts
@@ -0,0 +1,37 @@
+import { expect, describe, it, beforeEach } from "vitest";
+
+import { makeAnswer } from "test/factories/make-answer";
+import { InMemoryAnswerCommentsRepository } from "test/repositories/in-memory-answer-comments-repository";
+import { CommentOnAnswerUseCase } from "./comment-on-answer";
+import { InMemoryAnswersRepository } from "test/repositories/in-memory-answers-respository";
+
+let inMemoryAnswersRepository: InMemoryAnswersRepository;
+let inMemoryAnswerCommentsReposity: InMemoryAnswerCommentsRepository;
+let sut: CommentOnAnswerUseCase;
+
+describe("Comment on Answer", () => {
+  beforeEach(() => {
+    inMemoryAnswersRepository = new InMemoryAnswersRepository();
+    inMemoryAnswerCommentsReposity = new InMemoryAnswerCommentsRepository();
+    sut = new CommentOnAnswerUseCase(
+      inMemoryAnswersRepository,
+      inMemoryAnswerCommentsReposity,
+    );
+  });
+
+  it("should be able to comment on answer", async () => {
+    const answer = makeAnswer();
+
+    await inMemoryAnswersRepository.create(answer);
+
+    await sut.execute({
+      answerId: answer.id.toString(),
+      authorId: answer.authorId.toString(),
+      content: "Comentário teste",
+    });
+
+    expect(inMemoryAnswerCommentsReposity.items[0]?.content).toEqual(
+      "Comentário teste",
+    );
+  });
+});
