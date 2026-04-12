01d44b28de1d2a98264d433f8f6278360a1e5ad4
João Gabriel <joaogabriel9633@gmail.com>
2026-04-12 11:22:24 -0300

    update

---
+++ aulas/05-nest-clean/src/prisma/prisma.service.ts
@@ -0,0 +1,23 @@
+import "dotenv/config";
+import { Injectable, OnModuleDestroy, OnModuleInit } from "@nestjs/common";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "src/generated/prisma/client";
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  constructor() {
+    const adapter = new PrismaPg({
+      connectionString: process.env.DATABASE_URL as string,
+    });
+    super({ adapter, log: ["warn", "error"] });
+  }
+  onModuleInit() {
+    return this.$connect();
+  }
+  onModuleDestroy() {
+    return this.$disconnect();
+  }
+}
