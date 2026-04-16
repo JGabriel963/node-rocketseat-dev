929e2e2882e5b5f993842881e51c891fd1917165
João Gabriel <joaogabriel9633@gmail.com>
2026-04-16 14:03:03 -0300

    update

--- aulas/05-nest-clean/src/auth/auth.module.ts
+++ aulas/05-nest-clean/src/auth/auth.module.ts
@@ -3,21 +3,29 @@ import { ConfigService } from "@nestjs/config";
 import { JwtModule } from "@nestjs/jwt";
 import { PassportModule } from "@nestjs/passport";
 import { Env } from "src/env";
+import { JwtStrategy } from "./jwt.strategy";
 
 @Module({
   imports: [
     PassportModule,
     JwtModule.registerAsync({
       inject: [ConfigService],
+      global: true,
       useFactory: (config: ConfigService<Env, true>) => {
+        const privateKey = config.get("JWT_PRIVATE_KEY", { infer: true });
+        const publicKey = config.get("JWT_PUBLIC_KEY", { infer: true });
+
         return {
-          secret: config.get("JWT_SECRET", { infer: true }),
+          privateKey: Buffer.from(privateKey, "base64"),
+          publicKey: Buffer.from(publicKey, "base64"),
           signOptions: {
             expiresIn: "1d",
+            algorithm: "RS256",
           },
         };
       },
     }),
   ],
+  providers: [JwtStrategy],
 })
 export class AuthModule {}
