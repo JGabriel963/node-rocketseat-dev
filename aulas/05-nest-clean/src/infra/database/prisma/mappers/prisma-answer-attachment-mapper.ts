ba7cfa879ff610c2341fae39b31dc0174a1a9d53
João Gabriel <joaogabriel9633@gmail.com>
2026-05-04 15:22:13 -0300

    feat: implement comments and attachments database schema, entities, and repositories

---
+++ aulas/05-nest-clean/src/infra/database/prisma/mappers/prisma-answer-attachment-mapper.ts
@@ -0,0 +1,22 @@
+import {
+  Prisma,
+  Attachment as PrismaAttachment,
+} from "generated/prisma/client";
+import { UniqueEntityID } from "@/core/entities/unique-entity-id";
+import { AnswerAttachment } from "@/domain/forum/enterprise/entities/answer-attachment";
+
+export class PrismaAnswerAttachmentMapper {
+  static toDomain(raw: PrismaAttachment): AnswerAttachment {
+    if (!raw.answerId) {
+      throw new Error("Invalid attachment type.");
+    }
+
+    return AnswerAttachment.create(
+      {
+        attachmentId: new UniqueEntityID(raw.id),
+        answerId: new UniqueEntityID(raw.answerId),
+      },
+      new UniqueEntityID(raw.id),
+    );
+  }
+}
