4f5b203d0f5406c3be644f6fd1a55bf65edf2277
João Gabriel <joaogabriel9633@gmail.com>
2026-05-05 21:46:22 -0300

    feat: implement proxy service and module for request forwarding in api-gateway

---
+++ marketplace-ms/api-gateway/src/proxy/service/proxy.service.ts
@@ -0,0 +1,45 @@
+import { HttpService } from '@nestjs/axios';
+import { Injectable, Logger } from '@nestjs/common';
+import { firstValueFrom } from 'rxjs';
+import { serviceConfig } from 'src/config/gateway.config';
+
+@Injectable()
+export class ProxyService {
+  private readonly logger = new Logger(ProxyService.name);
+
+  constructor(private readonly httpService: HttpService) {}
+
+  async proxyRequest(
+    serviceName: keyof typeof serviceConfig,
+    method: string,
+    path: string,
+    data?: any,
+    headers?: any,
+    userInfo?: any,
+  ) {
+    const service = serviceConfig[serviceName];
+    const url = `${service.url}${path}`;
+
+    this.logger.log(`Proxying ${method} request to ${serviceName}: ${url}`);
+
+    try {
+      const enhancedHeaders = {
+        ...headers,
+        'x-user-id': userInfo?.userId,
+        'x-user-email': userInfo?.email,
+        'x-user-role': userInfo?.role,
+      };
+
+      const response = await firstValueFrom(
+        this.httpService.request<any>(method, url),
+      );
+    } catch (error) {
+      this.logger.error(
+        `Error proxying ${method} request to ${serviceName}: ${url}`,
+      );
+      throw error;
+    }
+  }
+
+  async getServiceHealth() {}
+}
